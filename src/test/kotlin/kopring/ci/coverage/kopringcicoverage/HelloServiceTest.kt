3c61016e53ea760910af160b43a544e80ae2f130
정보교 <yung12320@daum.net>
2022-12-15 20:18:07 +0900

    add: controller 테스트 작성

--- src/test/kotlin/kopring/ci/coverage/kopringcicoverage/HelloServiceTest.kt
+++ src/test/kotlin/kopring/ci/coverage/kopringcicoverage/HelloServiceTest.kt
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class KopringCiCoverageApplicationTests(
+class HelloServiceTest(
     @Autowired private val helloService: HelloService
 ) {
 

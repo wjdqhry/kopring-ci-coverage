1bd180af165a15cbcd3015972439a0db80d398d9
정보교 <yung12320@daum.net>
2022-12-15 13:06:58 +0900

    add: ci 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.1.0"
-    id("org.jetbrains.kotlinx.kover") version "0.6.1"
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.spring") version "1.7.21"
 
@@ -26,6 +25,23 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+val qDomains: MutableList<String> get() {
+    val qDomains = mutableListOf<String>()
+    for (qPattern in 'A' .. 'Z') {
+        qDomains.add("*.Q${qPattern}*")
+    }
+    return qDomains
+}
+val qDirectories: MutableList<String> get() {
+    val qDirectories = mutableListOf<String>()
+
+    for (qPattern in 'A' .. 'Z') {
+        qDomains.add("**/Q${qPattern}*")
+    }
+
+    return qDirectories
+}
+
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
@@ -35,73 +51,48 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 jacoco {
-    // JaCoCo 버전
-    toolVersion = "0.8.5"
+    toolVersion = "0.8.8"
+    reportsDirectory.set(layout.buildDirectory.dir("reportsJaCoCo"))
 }
 
 
 tasks.jacocoTestReport {
     reports {
-        // 원하는 리포트를 켜고 끌 수 있다.
-        html.isEnabled = true
-        xml.isEnabled = false
-        csv.isEnabled = false
-
-
-//      각 리포트 타입 마다 리포트 저장 경로를 설정할 수 있다.
-//      html.destination = file("$buildDir/jacocoHtml")
-//      xml.destination = file("$buildDir/jacoco.xml")
+        html.required.set(true)
+        xml.required.set(true)
+        csv.required.set(false)
     }
 
-    finalizedBy("jacocoTestCoverageVerification")
+    classDirectories.setFrom(
+        files(
+            classDirectories.files.map {
+                fileTree(it) {
+                    exclude(qDirectories + "**.*Application*")
+                }
+            }
+        )
+    )
+
+    finalizedBy(tasks.jacocoTestCoverageVerification)
 }
 
 
 tasks.jacocoTestCoverageVerification {
     violationRules {
         rule {
-            // 'element'가 없으면 프로젝트의 전체 파일을 합친 값을 기준으로 한다.
-            limit {
-                // 'counter'를 지정하지 않으면 default는 'INSTRUCTION'
-                // 'value'를 지정하지 않으면 default는 'COVEREDRATIO'
-                minimum = "0.30".toBigDecimal()
-            }
-        }
-
-        rule {
-            // 룰을 간단히 켜고 끌 수 있다.
             enabled = true
-
-            // 룰을 체크할 단위는 클래스 단위
             element = "CLASS"
 
-            // 브랜치 커버리지를 최소한 90% 만족시켜야 한다.
             limit {
-                counter = "BRANCH"
                 value = "COVEREDRATIO"
-                minimum = "0.30".toBigDecimal()
+                minimum = 0.7.toBigDecimal()
             }
 
-            // 커버리지 체크를 제외할 클래스들
-            excludes = listOf(
-//                    "*.test.*",
-//                    "*.Kotlin*"
-            )
+            excludes = qDomains + "**.*Application*"
         }
     }
-}
-
-val testCoverage by tasks.registering {
-    group = "verification"
-    description = "Runs the unit tests with coverage"
-
-    dependsOn(":test",
-        ":jacocoTestReport",
-        ":jacocoTestCoverageVerification")
-
-    tasks["jacocoTestReport"].mustRunAfter(tasks["test"])
-    tasks["jacocoTestCoverageVerification"].mustRunAfter(tasks["jacocoTestReport"])
 }
